51198d7a61573c92f9987ff3943882296c47b8b7
Erind Vora <erindvora@gmail.com>
2024-06-03 12:35:39 +0200

    versioni 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+class SquareCalculations {
+    // Function to calculate the perimeter of a square
+    public static double calculatePerimeter(double sideLength) {
+        return 4 * sideLength;
+    }
 
+    // Function to calculate the surface (area) of a square
+    public static double calculateArea(double sideLength) {
+        return sideLength * sideLength;
+    }
+
+    // Main function to demonstrate the usage of the above functions
     public static void main(String[] args) {
-        System.out.println("je shum i mir por je shum pis");
+        // Example side length of the square
+        double sideLength = 5.0;
+
+        // Calculate the perimeter using the calculatePerimeter function
+        double perimeter = calculatePerimeter(sideLength);
+
+        // Calculate the area using the calculateArea function
+        double area = calculateArea(sideLength);
+
+        // Print the results
+        System.out.println("Side length: " + sideLength);
+        System.out.println("Perimeter of the square: " + perimeter);
+        System.out.println("Area of the square: " + area);
     }
 }
