12c1e3e8a7776d2f514211e4bee9191715f0d460
Eugen Nikshiqi <eugnik21@htl-shkoder.com>
2024-06-03 12:40:19 +0200

    Funktionen zum Berechnen von Fläche, Umfang und Durchmesser eines Kreises hinzufügen
    - Funktion `flaecheninhaltKreis` implementieren, um die Fläche eines Kreises zu berechnen
    - Funktion `umfangKreis` implementieren, um den Umfang eines Kreises zu berechnen
    - Funktion `durchmesserKreis` implementieren, um den Durchmesser eines Kreises zu berechnen

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
     public static void main(String[] args) {
+        double radius = 5.0;
 
+        System.out.println("Flächeninhalt des Kreises: " + flaecheninhaltKreis(radius));
+        System.out.println("Umfang des Kreises: " + umfangKreis(radius));
+        System.out.println("Durchmesser des Kreises: " + durchmesserKreis(radius));
+    }
+
+    public static double flaecheninhaltKreis(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double umfangKreis(double radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    public static double durchmesserKreis(double radius) {
+        return 2 * radius;
     }
 }
