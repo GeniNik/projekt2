1d67dd19e827809c43474dbcb0fdfa11bc6e8db0
Eugen Nikshiqi <eugnik21@htl-shkoder.com>
2024-06-10 11:44:54 +0200

    2

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,73 @@ class SquareCalculations {
         return 4 * sideLength;
     }
 
-    // Function to calculate the surface (area) of a square
+    // Function to calculate the area of a square
     public static double calculateArea(double sideLength) {
         return sideLength * sideLength;
+    }
+}
+
+public class Main {
+    public static double flaecheninhaltKreis(double radius) {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public static double umfangKreis(double radius) {
+        return 2 * Math.PI * radius;
+    }
+
+    public static double durchmesserKreis(double radius) {
+        return 2 * radius;
+    }
+
+    // Funktion zur Berechnung des Flächeninhalts eines Drachenvierecks anhand der Diagonalen
+    public static double kiteAreaDiagonals(double d1, double d2) {
+        return (d1 * d2) / 2;
+    }
+
+    // Funktion zur Berechnung des Flächeninhalts eines Drachenvierecks anhand der Seitenlängen und des Winkels
+    public static double kiteAreaSidesAngle(double a, double b, double angleDegrees) {
+        double angleRadians = Math.toRadians(angleDegrees);
+        return a * b * Math.sin(angleRadians);
+    }
+
+    // Funktion zur Berechnung des Umfangs eines Drachenvierecks
+    public static double kitePerimeter(double a, double b) {
+        return 2 * (a + b);
     }
 
     // Main function to demonstrate the usage of the above functions
     public static void main(String[] args) {
         // Example side length of the square
         double sideLength = 5.0;
+        double radius = 5.0;
 
-        // Calculate the perimeter using the calculatePerimeter function
-        double perimeter = calculatePerimeter(sideLength);
+        System.out.println("Flächeninhalt des Kreises: " + flaecheninhaltKreis(radius));
+        System.out.println("Umfang des Kreises: " + umfangKreis(radius));
+        System.out.println("Durchmesser des Kreises: " + durchmesserKreis(radius));
 
-        // Calculate the area using the calculateArea function
-        double area = calculateArea(sideLength);
+        // Beispiele für die Nutzung der Funktionen
+        double d1 = 5;
+        double d2 = 6;
+        double areaDiagonals = kiteAreaDiagonals(d1, d2);
+        System.out.println("Flächeninhalt des Drachenvierecks (Diagonalen): " + areaDiagonals + " Einheiten²");
 
-        // Print the results
-        System.out.println("Side length: " + sideLength);
-        System.out.println("Perimeter of the square: " + perimeter);
-        System.out.println("Area of the square: " + area);
-        System.out.println("The calculatePerimeter and calculateArea functions are defined as methods of the SquareCalculations class.\n" +
-                "In the main method, the sideLength is set to 5.0 (this can be any value you want to test).\n" +
-                "The calculatePerimeter function is called with sideLength as an argument to get the perimeter of the square.\n" +
-                "The calculateArea function is called with sideLength as an argument to get the area of the square.\n" +
-                "The results are printed to the console.");
+        double a = 4;
+        double b = 3;
+        double angleDegrees = 60;
+        double areaSidesAngle = kiteAreaSidesAngle(a, b, angleDegrees);
+        System.out.println("Flächeninhalt des Drachenvierecks (Seiten und Winkel): " + areaSidesAngle + " Einheiten²");
+
+        double perimeterKite = kitePerimeter(a, b);
+        System.out.println("Umfang des Drachenvierecks: " + perimeterKite + " Einheiten");
+
+        // Calculate the perimeter and area of the square
+        double perimeterSquare = SquareCalculations.calculatePerimeter(sideLength);
+        double areaSquare = SquareCalculations.calculateArea(sideLength);
+
+        // Print the results for the square
+        System.out.println("Side length of the square: " + sideLength);
+        System.out.println("Perimeter of the square: " + perimeterSquare);
+        System.out.println("Area of the square: " + areaSquare);
     }
 }
